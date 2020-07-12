e0c6be81acf060a0b703498392700ac71e0c5944
lindaring <lndsimelane@gmail.com>
2020-07-12 20:16:02 +0200

    Added application yml and main class

---
+++ src/main/java/com/discovery/DiscoveryInterstellerApi.java
@@ -0,0 +1,11 @@
+package com.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryInterstellerApi {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryInterstellerApi.class, args);
+    }
+}
